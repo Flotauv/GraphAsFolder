1103b7184305f0442822f573bc252baaa4b3b005
chevalparesseux <mathisj3an@gmail.com>
2025-12-16 11:36:43 +0100

    jason

--- folder-json.py
+++ folder-json.py
@@ -11,4 +11,9 @@ def path_to_dict(path):
         d['type'] = "file"
     return d
 
-print json.dumps(path_to_dict(sys.argv[1]),  indent=4)
+# python folder-json.py .
+print(json.dumps(path_to_dict(sys.argv[1]), f, indent=4))
+
+if __name__ == "__main__":
+    with open("directory.json", "w", encoding="utf-8") as f:
+        json.dump(path_to_dict(sys.argv[1]), f, indent=4)
