d9338116c87243781c4af041a036da6ab34b7a49
Florent Saunier <florent.saunier13@gmail.com>
2025-12-17 17:08:33 +0100

    update graphique

--- src/pack.js
+++ src/pack.js
@@ -14,7 +14,38 @@ function getNodeColor(d, colorScale) {
 
     const baseColor = colorScale(ancestor.data.name);
     return d.children ? d3.rgb(baseColor).brighter(0.8) : baseColor;
-}
+};
+
+function zoomTo(v, label, node) {
+    const k = width / v[2];
+
+    view = v;
+
+    label.attr("transform", d => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`);
+    node.attr("transform", d => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`);
+    node.attr("r", d => d.r * k);
+  };
+
+// Fonction de zoom pour le pack
+function zoom(event, d, label) {
+    const focus0 = focus;
+
+    focus = d;
+
+    const transition = svg.transition()
+        .duration(event.altKey ? 7500 : 750)
+        .tween("zoom", d => {
+          const i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2]);
+          return t => zoomTo(i(t));
+        });
+
+    label
+      .filter(function(d) { return d.parent === focus || this.style.display === "inline"; })
+      .transition(transition)
+        .style("fill-opacity", d => d.parent === focus ? 1 : 0)
+        .on("start", function(d) { if (d.parent === focus) this.style.display = "inline"; })
+        .on("end", function(d) { if (d.parent !== focus) this.style.display = "none"; });
+  };
 
 
 // ========= Fonction principale pour charger les données ==============
@@ -184,10 +215,7 @@ function createPack(root){
     // Ajout des fichiers sous forme de cercles
     nodes.append("circle")
         .attr("r", d => d.r)
-        .attr("fill", d => {
-            if (d.depth === 0) return "none";
-            return getNodeColor(d, colorScale);
-        })
+        .attr("fill", d => d.children ? colorScale(d.depth) : "white")
         .attr("stroke", d => d.children ? "#fff" : "none")
         .attr("stroke-width", d => d.children ? 2 : 0)
         .style("opacity", d => d.depth === 0 ? 0 : (d.children ? 0.6 : 0.8));
@@ -198,4 +226,19 @@ function createPack(root){
         .delay((d, i) => i * 5)
         .style("opacity", 1);
 
+    // ajout des noms des fichiers
+    // Append the text labels.
+    const label = nodes.append("text")
+        .attr("text-anchor", "middle")
+        .attr("dy", "0.3em")
+        .style("font-size", d => Math.min(d.r / 3, 14) + "px")
+        .style("fill", "#fff")
+        .style("pointer-events", "none")
+        .style("font-weight", "600")
+        .text(d => d.r > 30 ? d.data.name : '');
+    
+
+    return svg.node();
+    
+    
 }
